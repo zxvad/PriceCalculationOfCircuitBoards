5d19e12cfdb1315cfd2e66f8b0cb528a6d4711f5
Dmitry Smirnov <dmitry.smirnov@fastdev.se>
2015-03-12 18:37:37 +0400

    миграции

---
+++ project/console/migrations/m000000_000000_initialDump.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`username` VARCHAR(60) NOT NULL,
+	`password_hash` VARCHAR(60) DEFAULT NULL,
+	`firstname` VARCHAR(50) NULL DEFAULT NULL,
+	`lastname` VARCHAR(50) NULL DEFAULT NULL
+	PRIMARY KEY (`id`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB;
---
+++ project/console/migrations/m141218_132558_init.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141218_132558_init extends Migration
+{
+    public function up()
+    {
+
+        $initialDbDump = file_get_contents(realpath(__DIR__) . '/m000000_000000_initialDump.sql');
+        return $this->execute($initialDbDump);
+
+    }
+
+    public function down()
+    {
+        $this->execute("
+		    SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT;
+            SET NAMES utf8;
+            SET FOREIGN_KEY_CHECKS=0;
+            SET UNIQUE_CHECKS=0;
+            SET AUTOCOMMIT=0;
+
+            DROP TABLE IF EXISTS `user`;
+
+		    COMMIT;
+            SET AUTOCOMMIT=1;
+            SET UNIQUE_CHECKS=1;
+            SET FOREIGN_KEY_CHECKS=1;
+            SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT;
+		");
+        return true;
+    }
+}
