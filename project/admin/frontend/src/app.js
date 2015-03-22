9f7caaeecfd537294c1d1d63d3338b0f31d6947a
Bentl <smdmitriyan@gmail.com>
2015-03-22 12:28:50 +0400

    изменил миграции

--- project/admin/frontend/src/app.js
+++ project/admin/frontend/src/app.js
@@ -2,9 +2,18 @@
 
 var app = angular.module('PriceCalculationOfCircuitBoards', ['ngRoute', 'ngAnimate', 'ngSanitize', 'mgcrea.ngStrap', 'ui.bootstrap']);
 
+app.constant('ACCESS_LEVELS', {
+    ROLE_ADMIN: [
+        'ADMIN'
+    ],
+    ROLE_USER: [
+        'USER',
+        'ADMIN'
+    ]
+});
 
-app.config(['$locationProvider', '$routeProvider', '$httpProvider', '$collapseProvider',
-    function ($locationProvider, $routeProvider, $httpProvider, $collapseProvider) {
+app.config(['$locationProvider', '$routeProvider', '$httpProvider', '$collapseProvider', 'ACCESS_LEVELS',
+    function ($locationProvider, $routeProvider, $httpProvider, $collapseProvider, ACCESS_LEVELS) {
 
     angular.extend($collapseProvider.defaults, {
         disallowToggle: true
@@ -14,7 +23,15 @@ app.config(['$locationProvider', '$routeProvider', '$httpProvider', '$collapsePr
     $routeProvider
         .when('/', {
             templateUrl: modulesPath + '/site/views/index.html',
-            controller: 'SiteController'
+            controller: 'SiteAuthController'
+        })
+        .when ('/login', {
+            templateUrl: modulesPath + '/site/views/login.html',
+            controller: 'SiteAuthController'
+        })
+        .when ('/404', {
+            templateUrl: modulesPath + '/site/views/errors/404.html',
+            name: '404'
         })
         .otherwise({redirectTo: '/404', name: '404'});
     $locationProvider.html5Mode(true).hashPrefix('!');
--- project/console/migrations/m000000_000000_initialDump.sql
+++ project/console/migrations/m000000_000000_initialDump.sql
@@ -2,9 +2,12 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`username` VARCHAR(60) NOT NULL,
+	`role` enum('USER', 'ADMIN') default NULL,
 	`password_hash` VARCHAR(60) DEFAULT NULL,
 	`firstname` VARCHAR(50) NULL DEFAULT NULL,
-	`lastname` VARCHAR(50) NULL DEFAULT NULL
+	`lastname` VARCHAR(50) NULL DEFAULT NULL,
+	`dep` integer,
+  `added_on` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
 )
 COLLATE='utf8_general_ci'
